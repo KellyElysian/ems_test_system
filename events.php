ef9e5bf024579bc6a8d3dc0d3875a9a1ceb59910
Kelly Dong <132804204+KellyElysian@users.noreply.github.com>
2024-02-03 18:17:37 -0500

    Changelog 1.2.4
    - Fixed CSS Font styling
    - Changed styling on the padding in the navigation bar

---
+++ events.php
@@ -0,0 +1,24 @@
+<?php
+// Automatically brings the config file
+require 'includes/config.php';
+
+// Regular SESSION variables for security reasons
+$member_id = $_SESSION['member_id'];
+$user_id = $_SESSION['user_id'];
+$user_role = $_SESSION['role'];
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Events</title>
+    <link rel="stylesheet" href="css/default.css">
+</head>
+
+<body>
+
+</body>
+
+</html>
--- home.php
+++ home.php
@@ -17,8 +17,6 @@ $user_role = $_SESSION['role'];
     <title>Home Page</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <!-- Font Stylesheet -->
-    <link href="https://fonts.googleapis.com/css2?family=Jaldi:wght@400;700&family=Mulish:ital,wght@0,300;0,400;0,700;1,300;1,700&family=Nunito:ital,wght@0,300;1,600&family=Overlock:wght@400;700;900&family=PT+Sans:wght@400;700&family=Roboto:wght@400;700&display=swap" rel="stylesheet">
     <!-- Home main stylesheet -->
     <link rel="stylesheet" href="css/default.css">
     <link rel="stylesheet" href="css/home.css">
