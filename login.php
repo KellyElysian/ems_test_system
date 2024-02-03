289767578cb8e166f8b8e786b079a69597009760
Kelly Dong <132804204+KellyElysian@users.noreply.github.com>
2024-02-03 17:55:57 -0500

    Changelog 1.2.1
    - Added more styling to login.php
    - login.php logging in is fully functional

--- login.php
+++ login.php
@@ -33,17 +33,63 @@ $login_submit = $_POST['login'];
     require 'includes/navbar.php';
     ?>
     <div class="container">
+        <?php
+        // Deals with login logic prior because if user enters the wrong credentials, it'll display form without needing to redundantly use two
+        // sections of a isset() where the form is present in both.
+        if (isset($login_submit)) {
+            // Login input variables
+            $log_email = isset($_POST['login_email']) ? $_POST['login_email'] : null;
+            $log_password = isset($_POST['login_password']) ? $_POST['login_password'] : null;
+
+            // Checks if the inputted email exists in the system
+            $email_query = mysqli_query($db_connection, "SELECT * FROM e_User WHERE email = '$log_email'");
+            $email_array = mysqli_fetch_assoc($email_query);
+            // Checks using a method that returns the number of rows (aka outputs) the query returned
+            // Could also use $email_array['email'] == $log_email to check if the emails exists.
+            if (mysqli_num_rows($email_query) > 0) {
+                // Checks the password using the email array
+                if ($email_array['password'] == $log_password) {
+                    // Grabbing the member associated with the logged in user's id
+                    $id = $email_array['uid'];
+                    $user_profile_query = mysqli_query($db_connection, "SELECT * FROM e_Member WHERE uid = $id");
+                    $user_profile_array = mysqli_fetch_assoc($user_profile_query);
+
+                    // Assigning session variables for when the user logs in.
+                    $_SESSION['user_id'] = $email_array['uid'];
+                    $_SESSION['role'] = $email_array['siteRole'];
+                    $_SESSION['member_id'] = $user_profile_array['id'];
+
+                    echo '<p class="success">Login successful, you will be redirected to the home page shortly.</p>';
+
+                    header('Refresh: 2; URL=https://cgi.luddy.indiana.edu/~keldong/ems/home.php');
+                    die();
+                } else {
+                    $password_check = true;
+                }
+            } else {
+                $email_check = true;
+            }
+        }
+        ?>
         <div class="form_container">
             <div>
                 <form method="POST">
+                    <?php
+                    if (isset($email_check)) {
+                        echo '<p class="error">Error: Email is not in our system.</p>';
+                    } else if (isset($password_check)) {
+                        echo '<p class="error">Error: Password is incorrect.</p>';
+                    }
+                    ?>
+
                     <div>
                         <label for="email">Email: </label>
-                        <input type="email" minlength="5" maxlength="25" required>
+                        <input type="email" name="login_email" minlength="5" maxlength="25" required>
                     </div>
 
                     <div>
                         <label for="password">Password:</label>
-                        <input type="password" minlength="6" maxlength="25" required>
+                        <input type="password" name="login_password" minlength="6" maxlength="25" required>
                     </div>
 
                     <input type="submit" name="login" class="submit login" value="Sign In">
