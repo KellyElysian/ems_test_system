7864e5758b770854295ba7c0aac2b58df80a7175
Kelly Dong <132804204+KellyElysian@users.noreply.github.com>
2024-02-13 15:56:13 -0500

    Changelog 1.7.3
    - Made sure if the user is not logged in, they do not receive notifications or alerts.

--- home.php
+++ home.php
@@ -2,13 +2,7 @@
 // Automatically brings the config file
 require 'includes/config.php';
 
-if ($_SESSION['reactivate'] == 1) {
-    echo '
-    <script>
-        alert("Ask an admin to reactivate your member status!");
-    </script>';
-    unset($_SESSION['reactivate']);
-}
+
 if ($_SESSION['no_perms'] == 1) {
     echo '
     <script>
@@ -16,53 +10,61 @@ if ($_SESSION['no_perms'] == 1) {
     </script>';
     unset($_SESSION['no_perms']);
 }
+if (isset($member_id)) {
+    if ($_SESSION['reactivate'] == 1) {
+        echo '
+    <script>
+        alert("Ask an admin to reactivate your member status!");
+    </script>';
+        unset($_SESSION['reactivate']);
+    }
+    /**
+     * Following code block is reused from profile.php to determine if the logged in user should have
+     * notices about their certications expiring soon.
+     */
+    // Grabs the CPR certification information if available.
+    $cpr_cert_query = mysqli_query($db_connection, "SELECT * FROM e_Cert_Assign WHERE member_id = $member_id AND cert_id = 99");
+    $cpr_cert_arr = mysqli_fetch_assoc($cpr_cert_query);
+    // Following deals with setting the expiration date and current date to get days till one's cert expires
+    date_default_timezone_set('America/Indiana/Indianapolis');
+    $expireDateDisplay = new DateTimeImmutable($cpr_cert_arr['expireDate']);
+    $expireDate = new DateTime($cpr_cert_arr['expireDate']);
+    $dateNow = date('Y-m-d');
+    $dateTimeNow = new DateTime($dateNow);
+    $tillExpire = $expireDate->diff($dateTimeNow)->format("%a");
 
-/**
- * Following code block is reused from profile.php to determine if the logged in user should have
- * notices about their certications expiring soon.
- */
-// Grabs the CPR certification information if available.
-$cpr_cert_query = mysqli_query($db_connection, "SELECT * FROM e_Cert_Assign WHERE member_id = $member_id AND cert_id = 99");
-$cpr_cert_arr = mysqli_fetch_assoc($cpr_cert_query);
-// Following deals with setting the expiration date and current date to get days till one's cert expires
-date_default_timezone_set('America/Indiana/Indianapolis');
-$expireDateDisplay = new DateTimeImmutable($cpr_cert_arr['expireDate']);
-$expireDate = new DateTime($cpr_cert_arr['expireDate']);
-$dateNow = date('Y-m-d');
-$dateTimeNow = new DateTime($dateNow);
-$tillExpire = $expireDate->diff($dateTimeNow)->format("%a");
-
-// Grabs the other certification needed for runs and stuff
-$other_cert_query = mysqli_query($db_connection, "SELECT * FROM e_Cert_Assign AS ca
+    // Grabs the other certification needed for runs and stuff
+    $other_cert_query = mysqli_query($db_connection, "SELECT * FROM e_Cert_Assign AS ca
 JOIN e_Cert AS c ON c.id = ca.cert_id
 WHERE member_id = $member_id AND cert_id != 99");
-$other_cert_arr = mysqli_fetch_assoc($other_cert_query);
-$other_cert_name = $other_cert_arr['name'];
-// Following deals with setting the expiration date and current date to get days till one's cert expires
-date_default_timezone_set('America/Indiana/Indianapolis');
-$o_expireDate = new DateTime($other_cert_arr['expireDate']);
-$o_expireDateDisplay = new DateTimeImmutable(($other_cert_arr['expireDate']));
-$o_dateNow = date('Y-m-d');
-$o_dateTimeNow = new DateTime($o_dateNow);
-$o_tillExpire = $o_expireDate->diff($o_dateTimeNow)->format("%a");
+    $other_cert_arr = mysqli_fetch_assoc($other_cert_query);
+    $other_cert_name = $other_cert_arr['name'];
+    // Following deals with setting the expiration date and current date to get days till one's cert expires
+    date_default_timezone_set('America/Indiana/Indianapolis');
+    $o_expireDate = new DateTime($other_cert_arr['expireDate']);
+    $o_expireDateDisplay = new DateTimeImmutable(($other_cert_arr['expireDate']));
+    $o_dateNow = date('Y-m-d');
+    $o_dateTimeNow = new DateTime($o_dateNow);
+    $o_tillExpire = $o_expireDate->diff($o_dateTimeNow)->format("%a");
 
 
-// Code block following with deal with storing a user's cert information in the session.
-// Checks if both certs are in the system
-if (mysqli_num_rows($cpr_cert_query) > 0 and mysqli_num_rows($other_cert_query) > 0) {
-    // Check if both certs are not expired
-    if ($tillExpire > 0 and $o_tillExpire > 0) {
-        // If both are not expired, sets valid certs to 1 (true) and sets a session variable for tracking
-        // what other cert besides the CPR the user has.
-        $_SESSION['certs_valid'] = 1;
-        $_SESSION['event_cert'] = $other_cert_arr['id'];
+    // Code block following with deal with storing a user's cert information in the session.
+    // Checks if both certs are in the system
+    if (mysqli_num_rows($cpr_cert_query) > 0 and mysqli_num_rows($other_cert_query) > 0) {
+        // Check if both certs are not expired
+        if ($tillExpire > 0 and $o_tillExpire > 0) {
+            // If both are not expired, sets valid certs to 1 (true) and sets a session variable for tracking
+            // what other cert besides the CPR the user has.
+            $_SESSION['certs_valid'] = 1;
+            $_SESSION['event_cert'] = $other_cert_arr['id'];
+        } else {
+            // If one is expired, sets valid certs to 0, which won't allot them to go on runs
+            $_SESSION['certs_valid'] = 0;
+        }
     } else {
-        // If one is expired, sets valid certs to 0, which won't allot them to go on runs
+        // If one of the certs are not in the system yet, certs are not valid.
         $_SESSION['certs_valid'] = 0;
     }
-} else {
-    // If one of the certs are not in the system yet, certs are not valid.
-    $_SESSION['certs_valid'] = 0;
 }
 ?>
 
@@ -85,55 +87,57 @@ if (mysqli_num_rows($cpr_cert_query) > 0 and mysqli_num_rows($other_cert_query) 
     <div class="main_container">
         <div class="notice_container">
             <?php
-            if (mysqli_num_rows($cpr_cert_query) > 0) {
-                if ($tillExpire <= 30 and $tillExpire > 0) {
+            if (isset($member_id)) {
+                if (mysqli_num_rows($cpr_cert_query) > 0) {
+                    if ($tillExpire <= 30 and $tillExpire > 0) {
             ?>
+                        <div class="warning_alert">
+                            <h4>CPR certification is expiring soon</h4>
+                            <p>Your CPR certification will expire in <?php echo $tillExpire . ' days on ' . $expireDateDisplay->format('n-j-y'); ?>.</p>
+                        </div>
+                    <?php
+                    } else if ($tillExpire <= 0) {
+                    ?>
+                        <div class="expired_alert">
+                            <h4>CPR certification is expired now</h4>
+                            <p>Your CPR certification expired on <?php echo $expireDateDisplay->format('n-j-y'); ?>.</p>
+                            <p>Please ensure to renew it or you will be unable to go on runs!</p>
+                        </div>
+                    <?php
+                    }
+                } else {
+                    ?>
                     <div class="warning_alert">
-                        <h4>CPR certification is expiring soon</h4>
-                        <p>Your CPR certification will expire in <?php echo $tillExpire . ' days on ' . $expireDateDisplay->format('n-j-y'); ?>.</p>
+                        <h4>Your CPR certification is not yet set</h4>
+                        <p>Please contact an admin as soon as possible to help set up your CPR certification.</p>
                     </div>
-                <?php
-                } else if ($tillExpire <= 0) {
-                ?>
-                    <div class="expired_alert">
-                        <h4>CPR certification is expired now</h4>
-                        <p>Your CPR certification expired on <?php echo $expireDateDisplay->format('n-j-y'); ?>.</p>
-                        <p>Please ensure to renew it or you will be unable to go on runs!</p>
-                    </div>
-                <?php
+                    <?php
                 }
-            } else {
-                ?>
-                <div class="warning_alert">
-                    <h4>Your CPR certification is not yet set</h4>
-                    <p>Please contact an admin as soon as possible to help set up your CPR certification.</p>
-                </div>
-                <?php
-            }
-            if (mysqli_num_rows($other_cert_query) > 0) {
-                if ($o_tillExpire <= 30 and $o_tillExpire > 0) {
-                ?>
+                if (mysqli_num_rows($other_cert_query) > 0) {
+                    if ($o_tillExpire <= 30 and $o_tillExpire > 0) {
+                    ?>
+                        <div class="warning_alert">
+                            <h4><?php echo $other_cert_name; ?> certification is expiring soon</h4>
+                            <p>Your <?php echo $other_cert_name; ?> certification will expire in <?php echo $o_tillExpire . ' days on ' . $o_expireDateDisplay->format('n-j-y'); ?>.</p>
+                        </div>
+                    <?php
+                    } else if ($o_tillExpire <= 0) {
+                    ?>
+                        <div class="expired_alert">
+                            <h4><?php echo $other_cert_name; ?> certification is expired now</h4>
+                            <p>Your <?php echo $other_cert_name; ?> certification expired on <?php echo $o_expireDateDisplay->format('n-j-y'); ?>.</p>
+                            <p>Please ensure to renew it or you will be unable to go on runs!</p>
+                        </div>
+                    <?php
+                    }
+                } else {
+                    ?>
                     <div class="warning_alert">
-                        <h4><?php echo $other_cert_name; ?> certification is expiring soon</h4>
-                        <p>Your <?php echo $other_cert_name; ?> certification will expire in <?php echo $o_tillExpire . ' days on ' . $o_expireDateDisplay->format('n-j-y'); ?>.</p>
+                        <h4>Your event/run certification is not yet set</h4>
+                        <p>Please contact an admin as soon as possible to help set up your event/run certification.</p>
                     </div>
-                <?php
-                } else if ($o_tillExpire <= 0) {
-                ?>
-                    <div class="expired_alert">
-                        <h4><?php echo $other_cert_name; ?> certification is expired now</h4>
-                        <p>Your <?php echo $other_cert_name; ?> certification expired on <?php echo $o_expireDateDisplay->format('n-j-y'); ?>.</p>
-                        <p>Please ensure to renew it or you will be unable to go on runs!</p>
-                    </div>
-                <?php
-                }
-            } else {
-                ?>
-                <div class="warning_alert">
-                    <h4>Your event/run certification is not yet set</h4>
-                    <p>Please contact an admin as soon as possible to help set up your event/run certification.</p>
-                </div>
             <?php
+                }
             }
             ?>
         </div>
